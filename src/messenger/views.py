5e31eb663dd42adf06c630ebfe6283fb2c64953a
caplanmaor <caplanmaor@gmail.com>
2022-05-28 13:48:39 +0300

    add feature to send messages

--- src/messenger/views.py
+++ src/messenger/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login
+from messenger.models import Message
 
+# import json
 @csrf_exempt
 def create_user(request):
     if request.method == "POST":
@@ -12,23 +14,29 @@ def create_user(request):
         password = request.POST['password']
         user = User.objects.create_user(username, email, password)
         user.save()
-        return('200 - user created')
+        return HttpResponse(status=200)
 
 @csrf_exempt
-def login(request):
+def login_user(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return('200 - user logged in')
+        login(request, user)
+        return HttpResponse(status=200)
 
-#csrf_exempt
+@csrf_exempt
 def send_message(request):
     if request.method == "POST":
         if request.user.is_authenticated:
-            print('logged in')
+            current_user = request.user
+            title = request.POST['title']
+            body = request.POST['body']
+            receiver = request.POST['receiver']
+            rec_user = User.objects.get(username=receiver)
+            message = Message.objects.create(title=title, body=body, owner_id=current_user.id, receiver_id=rec_user.id)
+            message.save()
+            return HttpResponse(status=200)
         else:       
             # Do something for anonymous users.
-             print('plesae log in')
+            return HttpResponse(status=500)
